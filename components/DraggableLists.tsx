9f8e4bea759ed70a12fa0b64c109c4a6072e8fa0
Silverium <soldeplatadeveloper@gmail.com>
2023-11-10 10:15:26 +0100

    Add row reordering functionality to DraggableLists component

--- components/DraggableLists.tsx
+++ components/DraggableLists.tsx
@@ -42,6 +42,13 @@ export default function DraggableLists({ products }: { products: Product[] }) {
         }
 
         console.log(result.destination)
+        if(result.type === "row") {
+            const clonedItems = [...items.map(row => [...row])];
+            const [removed] = clonedItems.splice(result.source.index, 1);
+            clonedItems.splice(result.destination.index, 0, removed);
+            updateItems(clonedItems);
+            return;
+        }
         const clonedItems = [...items.map(row => [...row])];
         const sourceRow = Number(result.source.droppableId);
         const destRow = Number(result.destination.droppableId);
@@ -79,49 +86,72 @@ export default function DraggableLists({ products }: { products: Product[] }) {
                     Clear empty rows
                 </button>
             </div>
-
-            <DragDropContext onDragEnd={onDragEnd} onDragStart={() => console.log("drag start")}
-                onDragUpdate={(result) => console.log("drag update", result)}>
-                {items.map((row, rowIndex) => (
-                    <Droppable key={rowIndex} droppableId={`${rowIndex}`} direction="horizontal">
+            <div><h2>Parent of draggable lists</h2>
+                <DragDropContext onDragEnd={onDragEnd} onDragStart={() => console.log("drag start")}
+                    onDragUpdate={(result) => console.log("drag update", result)}>
+                    <Droppable droppableId="all-rows" direction="vertical" type="row">
                         {(provided, snapshot) => (
-                            <div ref={provided.innerRef} className={`flex ${snapshot.isDraggingOver ? "bg-red-500" : "bg-blue-500"} min-h-[200px]`} {...provided.droppableProps} >
+                            <div ref={provided.innerRef} className={`${snapshot.isDraggingOver ? "bg-yellow-500" : "bg-purple-500"} min-h-[200px]`} {...provided.droppableProps} >
+                                {items.map((row, rowIndex) => (
+                                    <Draggable key={rowIndex} draggableId={`${rowIndex}`} index={rowIndex}>
+                                        {(provided) => (
+                                            <div
+                                                {...provided.draggableProps}
+                                                ref={provided.innerRef}
+                                                className="draggable-row"
+                                            >
+                                                <h3 {...provided.dragHandleProps}>{`Row ${rowIndex + 1}`}</h3>
 
-                                {row.map((product, index) => (
-                                    <Draggable key={product.id} draggableId={(product.name)} index={index}>
-                                        {(provided, snapshot) => {
-                                            return (
-                                                <div ref={provided.innerRef}
-                                                    {...provided.draggableProps}
-                                                    {...provided.dragHandleProps}
-                                                    className={`m-2`}
-                                                >
-                                                    <Link
-                                                        href={{ pathname: "/products", query: { products: [product.id] } }}
-                                                        className="rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                                                        rel="noopener noreferrer"
-                                                    >
-                                                        {product.name}
-                                                    </Link>
-                                                    <img src={`${imageLinks[product.id]}`} alt={product.name} width={product.imageWidth} />
-                                                    <span>{product.price}€</span>
-                                                </div>
-                                            )
-                                        }}
+                                                <Droppable key={rowIndex} droppableId={`${rowIndex}`} direction="horizontal">
+                                                    {(provided, snapshot) => (
+                                                        <div ref={provided.innerRef} className={`flex ${snapshot.isDraggingOver ? "bg-red-500" : "bg-blue-500"} min-h-[200px]`} {...provided.droppableProps} >
+
+                                                            {row.map((product, index) => (
+                                                                <Draggable key={product.id} draggableId={(product.name)} index={index}>
+                                                                    {(provided, snapshot) => {
+                                                                        return (
+                                                                            <div ref={provided.innerRef}
+                                                                                {...provided.draggableProps}
+                                                                                {...provided.dragHandleProps}
+                                                                                className={`m-2`}
+                                                                            >
+                                                                                <Link
+                                                                                    href={{ pathname: "/products", query: { products: [product.id] } }}
+                                                                                    className="rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                                                                                    rel="noopener noreferrer"
+                                                                                >
+                                                                                    {product.name}
+                                                                                </Link>
+                                                                                <img src={`${imageLinks[product.id]}`} alt={product.name} width={product.imageWidth} />
+                                                                                <span>{product.price}€</span>
+                                                                            </div>
+                                                                        )
+                                                                    }}
+                                                                </Draggable>
+                                                            ))}
+                                                            {provided.placeholder}
+                                                            {row.length === 0
+                                                                ? (<button className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                                                                    onClick={removeRow(rowIndex)}>
+                                                                    remove row</button>)
+                                                                : (<button className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                                                                    onClick={addRow(rowIndex)}>
+                                                                    add row</button>)}
+                                                        </div>
+                                                    )}
+                                                </Droppable>
+                                            </div>
+                                        )}
                                     </Draggable>
                                 ))}
                                 {provided.placeholder}
-                                {row.length === 0
-                                    ? (<button className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                                    onClick={removeRow(rowIndex)}>
-                                        remove row</button>)
-                                    : (<button className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                                    onClick={addRow(rowIndex)}>
-                                        add row</button>)}
                             </div>
                         )}
-                    </Droppable>))}
-            </DragDropContext>
+                    </Droppable>
+
+                </DragDropContext>
+
+            </div>
         </div>
 
     )
