14e1d5cf544d23a0b2c793a326617ae89872ea51
Silverium <soldeplatadeveloper@gmail.com>
2023-11-13 11:41:01 +0100

    Add grid editor requirements documentation

--- README.md
+++ README.md
@@ -36,6 +36,18 @@ The easiest way to deploy your Next.js app is to use the [Vercel Platform](https
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
 
 # About the exercise
+## Requirements
+- The photo, name, and price of each product must be visible at all times.
+- As many rows as desired can be created as long as there are elements in the row.
+- Rows must have between 1 and 3 elements.
+- Users must be able to add elements to rows and exchange them between different created rows or between the elements of the same row using drag and drop.
+- Users must be able to move rows of position.
+- You can zoom in and out on the grid editor. If you add many rows, you lose the context of how the grid would look. Zoom makes it easy to see the maximum number of rows possible. This zoom should only be done on the editor area and not on the entire page, so you **cannot** use the native browser zoom.
+- A template can be associated and disassociated with a row.
+- Users must see the name of the template that a particular row has.
+- In addition to seeing the name, users must see from the application how the alignment would look according to the template they have selected. In other words, if the right-alignment template is selected, the products must be aligned to the right in the editor.
+- Users can save the grid, but all rows must have products and all rows must have a template assigned.
+
 ## Steps followed
 1. Fork a vercel's repo with a basic template. Remove unnecessary stuff.
 2. Create the mock Products service
@@ -49,6 +61,7 @@ Check out our [Next.js deployment documentation](https://nextjs.org/docs/deploym
 10. Create the `/templates` endpoint and service
 11. Create templates selector button
 12. Zoom functionality to Products Page
- - I found a bug in the `beautiful-drag-n-drop` functionality that does not allow for scaled items to be dragged around. I decided to disable the feature while scaling.
+    - I found a bug in the `beautiful-drag-n-drop` functionality that does not allow for scaled items to be dragged around. I decided to disable the feature while scaling.
 13. Create the `/grids` endpoint to POST grids and to GET grids
 14. Add updateGrid function and refactor createGrid to handle updates
+15. Document the requirements of the exercise
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-start m-24 gap-8">
       <div className="gap-6 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           This is a coding exercise for a job application.
@@ -14,90 +14,38 @@ export default function Home() {
           </span>
         </div>
       </div>
-      {/* <div className="relative flex place-items-center" >
-        <Link
-          href={{ pathname: "/products", query: { products: [1, 2, 3, 4, 5] } }}
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          rel="noopener noreferrer"
-        >
-          5 Products
-        </Link>
-      </div> */}
-
-
-      {/* To be removed in later stages: */}
-      {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        by Soldeveloper
-      </div> */}
-      {/* <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div> */}
+      <ul className="gap-4 max-w-5xl w-full items-center justify-between font-mono text-sm">
+        <li className="flex gap-4 max-w-5xl w-full items-center justify-between font-mono text-sm">
+          The photo, name, and price of each product must be visible at all times.
+        </li>
+        <li className="flex gap-4 max-w-5xl w-full items-center justify-between font-mono text-sm">
+          As many rows as desired can be created as long as there are elements in the row.
+        </li>
+        <li className="flex gap-4 max-w-5xl w-full items-center justify-between font-mono text-sm">
+          Rows must have between 1 and 3 elements.
+        </li>
+        <li className="flex gap-4 max-w-5xl w-full items-center justify-between font-mono text-sm">
+          Users must be able to add elements to rows and exchange them between different created rows or between the elements of the same row using drag and drop.
+        </li>
+        <li className="flex gap-4 max-w-5xl w-full items-center justify-between font-mono text-sm">
+          Users must be able to move rows of position.
+        </li>
+        <li className="flex gap-4 max-w-5xl w-full items-center justify-between font-mono text-sm">
+          You can zoom in and out on the grid editor. If you add many rows, you lose the context of how the grid would look. Zoom makes it easy to see the maximum number of rows possible. This zoom should only be done on the editor area and not on the entire page, so you **cannot** use the native browser zoom.
+        </li>
+        <li className="flex gap-4 max-w-5xl w-full items-center justify-between font-mono text-sm">
+          A template can be associated and disassociated with a row.
+        </li>
+        <li className="flex gap-4 max-w-5xl w-full items-center justify-between font-mono text-sm">
+          Users must see the name of the template that a particular row has.
+        </li>
+        <li className="flex gap-4 max-w-5xl w-full items-center justify-between font-mono text-sm">
+          In addition to seeing the name, users must see from the application how the alignment would look according to the template they have selected. In other words, if the right-alignment template is selected, the products must be aligned to the right in the editor.
+        </li>
+        <li className="flex gap-4 max-w-5xl w-full items-center justify-between font-mono text-sm">
+          Users can save the grid, but all rows must have products and all rows must have a template assigned.
+        </li>
+      </ul>
     </main>
   )
 }
