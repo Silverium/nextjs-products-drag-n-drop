34becebac5fca90de0e0736e0f7a96821e08bf6a
Silverium <soldeplatadeveloper@gmail.com>
2023-11-13 15:52:38 +0100

    Fix type error in product route

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -4,7 +4,7 @@ import { type NextRequest } from "next/server";
  // exposed API route to get products by id. I am not sure if it will be needed.
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
-  const ids = (searchParams.getAll("id") || []) as unknown as number[];
+  const ids = (searchParams.getAll("id") || []) as unknown as string[];
   const products = await getProducts(ids);
   return Response.json(products);
 }
